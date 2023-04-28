6b935e2663c292885a97990e732548fdb90fa9c7
PashkovskyaYY <PashkovskyaYY@mpei.ru>
2023-04-28 13:10:26 +0300

    2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-begin';
+  inputValueStr = ""
+
+  image = "https://www.worldatlas.com/r/w1200/upload/c7/91/96/shutterstock-109340948.jpg"
+
+  changeTitleAndImg(): void{
+
+    this.title = "Changed title"
+    this.image = "https://www.wallpaperflare.com/static/953/87/674/fat-cat-lying-light-tortoiseshell-wallpaper.jpg"
+  }
+
+  inputValue(event: Event): void{
+    //console.log("changed", event)
+    this.inputValueStr = (event.target as HTMLInputElement).value;
+  };
 }
